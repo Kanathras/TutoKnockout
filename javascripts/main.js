21ab04429972a97256a1d54e313e1ba6d154b372
Cyril Dupuis <dupuis.cyril@gmail.com>
2013-02-05 17:27:35 +0100

    Reset to empty project (easier to follow the tutorial)

--- javascripts/main.js
+++
@@ -1,9 +0,0 @@
-define(["vendors/knockout-2.1.0", "viewmodels/board", "router", "bindings/sortable"], function(ko, board, router) {
-
-	var boardElement = document.getElementById("main");
-
-	ko.applyBindings(board, boardElement);
-
-	router.run("#/");
-
-});
